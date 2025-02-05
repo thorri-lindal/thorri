07bbf649f6cb8d04fca7154e1c6d74b9ba96e13a
Þorri Líndal Guðnason <jormunvakur@tuta.io>
2025-02-05 16:22:35 +0000

    Replace footer function with Component.Footer in Quartz layout

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -23,7 +23,7 @@ export const sharedPageComponents: SharedLayout = {
       }
     }),
   ],
-  footer: () => ({ css: "" }),
+  footer: Component.Footer(),
 }
 
 // components for pages that display a single page (e.g. a single note)
