c5d502675cf68fa8d3cdfa9b70332ecc64648eeb
Þorri Líndal Guðnason <jormunvakur@tuta.io>
2025-02-05 02:31:42 +0000

    fix

---
+++ content/Testing Markdown.md
@@ -0,0 +1,17 @@
+> [!info] 
+>  Here's a callout block. 
+>  It supports **Markdown**, [[Internal link|Wikilinks]], and [[Embed files|embeds]]!
+
+>[!bug]
+> This is not supposed to happen!
+
+> [!NOTE] Title
+> This project is still under development, please check in from time to time.
+
+> [!example] Execution
+> ./sourcing.sh
+> python3 sourcing.py --help
+
+Hallmar er sætur
+Hallmar er bestur
+Hallmar er gangster
--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,23 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+    afterBody: [
+      Component.Comments({
+        provider: 'giscus',
+        options: {
+          // from data-repo
+          repo: 'thorri-lindal/thorri-lindal.github.io',
+          // from data-repo-id
+          repoId: 'R_kgDON0eX8Q',
+          // from data-category
+          category: 'Announcements',
+          // from data-category-id
+          categoryId: 'DIC_kwDON0eX8c4Cmqyq',
+        }
+      }),
+    ],
+  ],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/jackyzha0/quartz",
